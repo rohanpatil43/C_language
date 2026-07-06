7765a68e4a00f709009df59f15b1ad570cac9501
rohanpatil43 <rp0707007@gmail.com>
2026-07-06 20:06:55 +0530

    add new file

---
+++ Fibonacci.c
@@ -0,0 +1,19 @@
+//Fibonacci Serise
+ #include<stdio.h>
+   int main(){
+   	
+   	int n;
+   	printf("Enter a number :");
+   	scanf("%d" ,&n);
+   	
+   	int a=1;
+   	int b=1;
+   	int sum=1;
+   	 int i;
+   	 for(i=1;i<=n-2;i++){
+   	 	sum=a+b;
+   	 	a=b;
+   	 	b=sum;
+		}
+     printf("The %dth Fibonacci Serise is %d" ,n,sum);
+}
