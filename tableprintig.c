6c31e6227719744311b869e6c57c366cea5b83f5
rohanpatil43 <rp0707007@gmail.com>
2026-07-16 14:52:26 +0530

    table of any number

---
+++ tableprintig.c
@@ -0,0 +1,17 @@
+//Table prenting
+#include<stdio.h>
+  int main(){
+  	int num; 
+  	printf("ENTER A INTEGER TO PRINT ITS TABLE :");
+  	scanf("%d",&num);
+  	printf("Multatiplaction Table %d" ,num);
+    int i;
+  	for(i=1;i<=10;i++){
+  		{
+  		printf("\n %d " ,num *i);
+  	}
+		 
+} 
+return 0;
+}
+ 
